ce66e38946f987baae5303bdeadf7388d139136d
mbrsagor <mbrsagor@gmail.com>
2024-05-14 23:18:57 +0600

    New: Order DB completed

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from delivery.models import *
 
-# Register your models here.
+admin.site.register([Service, CreateOrder])
--- delivery/models.py
+++ delivery/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+
+
+class Service(TimeStampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Format(TimeStampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CreateOrder(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='create_order')
+    services = models.ManyToManyField(Service, related_name='OrderServices')
+    notes = models.TextField(blank=True, default='')
+    download_link = models.URLField(max_length=555)
+    is_complete = models.BooleanField(default=False)
+    is_paid = models.BooleanField(default=False)
+    delivery_date = models.DateField()
+    delivery_time = models.TimeField()
+
+    def __str__(self):
+        return self.user.username
